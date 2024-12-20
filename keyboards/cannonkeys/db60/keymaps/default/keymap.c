60c59cda4ec3e827023c68091c0a273acd19d46b
PhilipHsu609 <mpyhacct118113@gmail.com>
2024-12-20 22:33:00 +0800

    Change the QK_BOOT position in default keymap

--- keyboards/cannonkeys/db60/keymaps/default/keymap.c
+++ keyboards/cannonkeys/db60/keymaps/default/keymap.c
@@ -16,7 +16,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 #include QMK_KEYBOARD_H
 
-
 // Each layer gets a name for readability, which is then used in the keymap matrix below.
 // The underscores don't mean anything - you can have a layer called STUFF or any other name.
 // Layer names don't all need to be of the same length, obviously, and you can also skip them
@@ -32,7 +31,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,   KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC,  KC_BSLS,
     KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,                 KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,           KC_ENT,
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,                 KC_M,    KC_COMM, KC_DOT,  KC_SLSH,                    KC_RSFT,
-    KC_LCTL, KC_LGUI, KC_LALT,                            KC_SPC,                                          KC_RALT, KC_RGUI, MO(_FN1), KC_RCTL
+    KC_LCTL, KC_LGUI, KC_LALT,                            KC_SPC,                                          KC_RALT, KC_RGUI, KC_RCTL,  MO(_FN1)
   ),
 
   [_FN1] = LAYOUT_60_ansi(
@@ -40,6 +39,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     UG_TOGG, UG_NEXT, KC_UP,   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
     BL_BRTG, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______, _______, _______, _______, _______, _______, _______,          _______,
     BL_UP,   BL_DOWN, BL_TOGG, _______, _______, _______, _______, _______, _______, _______, _______,                   _______,
-    KC_GRV,  _______, _______,                            _______,                            _______, _______, _______, QK_BOOT
+    QK_BOOT, _______, _______,                            _______,                            _______, _______, _______, _______
   )
 };
