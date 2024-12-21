a8d67de9056e51d7988e2d5858cf65ec1dd57e78
PhilipHsu609 <mpyhacct118113@gmail.com>
2024-12-21 15:07:09 +0800

    Just like katana!

---
+++ keyboards/cannonkeys/db60/keymaps/philip/config.h
@@ -0,0 +1,13 @@
+//
+#pragma once
+
+#define NO_DEBUG
+#define NO_PRINT
+
+#define MOUSEKEY_INERTIA
+#define MOUSEKEY_DELAY 100
+#define MOUSEKEY_INTERVAL 16
+#define MOUSEKEY_MAX_SPEED 32
+#define MOUSEKEY_TIME_TO_MAX 30
+#define MOUSEKEY_FRICTION 20
+#define MOUSEKEY_MOVE_DELTA 1
---
+++ keyboards/cannonkeys/db60/keymaps/philip/rules.mk
@@ -0,0 +1,10 @@
+COMMAND_ENABLE = no
+DYNAMIC_MACRO_ENABL = no # Record/Replay dynamic macros
+BACKLIGHT_ENABLE = no
+RGBLIGHT_ENABLE = no
+
+# Wildcard to allow APM32 MCU
+DFU_SUFFIX_ARGS = -v FFFF -p FFFF
+
+DEFAULT_FOLDER = cannonkeys/db60/hotswap
+BOOTLOADER = apm32-dfu
